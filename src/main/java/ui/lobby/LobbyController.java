309faa066edd85e83f8f2993536442de66a5f6a6
LetteraUnica <lorenzocav97@gmail.com>
2022-11-21 19:32:57 +0100

    Implemented return to menu button

--- src/main/java/ui/lobby/LobbyController.java
+++ src/main/java/ui/lobby/LobbyController.java
@@ -1,11 +1,27 @@
 package ui.lobby;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.stage.Stage;
+import ui.mainMenu.MainMenu;
+
+import javax.swing.text.html.ListView;
 
 public class LobbyController {
-
+    @FXML
+    public ListView playerList;
     @FXML
     public void addPlayer() {
-        System.out.println("Add player pressed");
+        // TODO: Add player to playerList
+    }
+    @FXML
+    public void returnToMainMenu(ActionEvent e) throws Exception {
+        MainMenu mainMenu = new MainMenu();
+        mainMenu.showStage((Stage) ((Node) e.getSource()).getScene().getWindow());
+    }
+    @FXML
+    public void startGame() {
+        // TODO: Implement startGame
     }
 }
