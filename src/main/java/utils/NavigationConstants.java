2455617fbfa158fdb30613d6bcc545171403ff2a
LetteraUnica <lorenzocav97@gmail.com>
2022-11-22 01:53:35 +0100

    Create NavigationConstants.java

---
+++ src/main/java/utils/NavigationConstants.java
@@ -0,0 +1,6 @@
+package utils;
+
+public class NavigationConstants {
+    public static final String STARTING_MENU_FXML = "mainMenu.fxml";
+    public static final String LOBBY_FXML = "lobby.fxml";
+}
