6467845b15952a7f82c1217a4e6cbda3a0a1373f
LetteraUnica <lorenzocav97@gmail.com>
2022-11-22 01:54:19 +0100

    use navigator in main

--- src/main/java/ui/Main.java
+++ src/main/java/ui/Main.java
@@ -2,16 +2,16 @@ package ui;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
-import ui.mainMenu.MainMenu;
+import ui.navigator.Navigator;
+import utils.NavigationConstants;
 
 public class Main extends Application {
-
     public static void main(String[] args) {
         launch(args);
     }
     @Override
     public void start(Stage primaryStage) throws Exception {
-        MainMenu mainMenu = new MainMenu();
-        mainMenu.showStage(primaryStage);
+        Navigator navigator = new Navigator();
+        navigator.navigateTo(primaryStage, NavigationConstants.STARTING_MENU_FXML);
     }
 }
