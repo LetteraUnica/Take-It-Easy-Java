ad53b04c29de6bd35143c3a2bfdde2eced80adfc
LetteraUnica <lorenzocav97@gmail.com>
2022-11-23 16:32:17 +0100

    revert changes to TileListLoader

--- src/main/java/beans/TileListLoader.java
+++ src/main/java/beans/TileListLoader.java
@@ -9,7 +9,6 @@ import java.io.FileReader;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 public class TileListLoader {
 
@@ -21,15 +20,9 @@ public class TileListLoader {
 
     public void loadTileList(String filename) throws FileNotFoundException {
         BufferedReader file = new BufferedReader(new FileReader(filename));
-        Type jsonListType = new TypeToken<List<Map<String, Integer>>>(){}.getType();
-        List<Map<String, Integer>> jsonList = new Gson().fromJson(file, jsonListType);
-        listMapToTileList(jsonList);
-    }
-
-    private void listMapToTileList(List<Map<String, Integer>> jsonList) {
-        for (Map<String, Integer> tileMap: jsonList) {
-            allTiles.add(new Tile(tileMap.get("idTile"), tileMap.get("leftPath"), tileMap.get("rightPath"), tileMap.get("topPath")));
-        }
+        Type jsonListType = new TypeToken<List<Tile>>(){}.getType();
+        List<Tile> jsonList = new Gson().fromJson(file, jsonListType);
+        allTiles.addAll(jsonList);
     }
 
     public ArrayList<Tile> getTileList() {
