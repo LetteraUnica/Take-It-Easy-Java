e9ecb516b41895daa44f6a90a69ae7e7b9b48260
Marco Sicklinger <you@example.com>
2022-12-04 17:21:15 +0400

    Implementation of some tile controlling methods in MatchState

--- src/main/java/engine/state/MatchState.java
+++ src/main/java/engine/state/MatchState.java
@@ -1,18 +1,20 @@
 package engine.state;
 
-import engine.model.board.Board;
-import engine.model.tile.Tile;
+import engine.model.board.BoardInterface;
+import engine.model.tile.TileInterface;
 import exceptions.FatalGameErrorException;
 import utils.TileLoader;
 
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class MatchState {
-    private HashMap<Integer, Board> boards;
-    private List<Tile> tileCollection;
+    private HashMap<Integer, BoardInterface> boards;
+    private ArrayList<TileInterface> tileCache;
+    private TileInterface currentTile;
 
     public MatchState() throws FatalGameErrorException {
         try {
@@ -20,36 +22,40 @@ public class MatchState {
         } catch (FileNotFoundException e) {
             throw new FatalGameErrorException();
         }
-
     }
 
     private void initializeTileCollection() throws FileNotFoundException {
         TileLoader tileLoader = new TileLoader();
-        this.tileCollection = tileLoader.loadTileList();
-        ;
+        tileCache.addAll(tileLoader.loadTileList());
     }
 
-    public void addBoard(Board board) {
-
+    public Integer getCacheSize() {
+        return tileCache.size();
     }
 
-    public void deleteBoard(String name) {
-
+    public void drawTile() {
+        int chosenTileIndex = ThreadLocalRandom.current().nextInt(getCacheSize() + 1);
+        currentTile = tileCache.remove(chosenTileIndex);
     }
 
-    public void renameBoard(String oldName, String newName) {
-
+    public void addBoard(BoardInterface board) {
+        boards.put(boards.size() + 1, board);
     }
 
-    public Board getBoard(String name) {
-        return (Board) boards.get(name);
+    public void deleteBoard(Integer playerId) {
+        boards.entrySet().removeIf(entry -> entry.getKey().equals(playerId));
     }
 
-    public Map getBoards() {
+    public BoardInterface getBoard(Integer playerId) {
+        return (BoardInterface) boards.get(playerId);
+    }
+
+    public Map<Integer, BoardInterface> getBoards() {
         return boards;
     }
 
     public int getNumberOfPlayers() {
         return boards.size();
     }
+
 }
