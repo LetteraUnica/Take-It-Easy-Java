dc1791fd33eac2c51635510b03e32ec5411deb1b
Lorenzo Cavuoti <lorenzocav97@gmail.com>
2022-12-16 01:28:09 +0100

    added icon

--- src/main/java/ui/Main.java
+++ src/main/java/ui/Main.java
@@ -1,9 +1,12 @@
 package ui;
 
 import javafx.application.Application;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import ui.navigator.NavigationConstants;
 import ui.navigator.Navigator;
+
+import java.util.Objects;
 
 public class Main extends Application {
     public static void main(String[] args) {
@@ -12,7 +15,13 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        setLogo(primaryStage);
         Navigator navigator = new Navigator();
         navigator.navigateTo(primaryStage, NavigationConstants.STARTING_MENU_FXML);
+    }
+
+    private void setLogo(Stage primaryStage) {
+        Image icon = new Image(Objects.requireNonNull(getClass().getResource("icon.png")).toString());
+        primaryStage.getIcons().add(icon);
     }
 }
