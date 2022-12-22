7adddb18bd0a876afb0c5683db7c56632cb6a46c
Lorenzo Cavuoti <lorenzocav97@gmail.com>
2022-12-22 22:33:33 +0100

    moved methods to UIUtils

--- src/main/java/utils/ui/UIUtils.java
+++ src/main/java/utils/ui/UIUtils.java
@@ -5,6 +5,7 @@ import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.control.OverrunStyle;
 import javafx.scene.layout.Pane;
+import javafx.scene.shape.Polygon;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
@@ -27,5 +28,21 @@ public class UIUtils {
         rowContainer.setPadding(new javafx.geometry.Insets(20, 0, 20, 0));
         rowContainer.setPrefHeight(30);
         return rowContainer;
+    }
+
+    public static void setPolygonSides(Polygon polygon, double radius, int sides) {
+        polygon.getPoints().clear();
+        final double angleStep = Math.PI * 2 / sides;
+        double angle = angleStep / 2;
+        for (int i = 0; i < sides; i++, angle += angleStep) {
+            polygon.getPoints().addAll(
+                    Math.sin(angle) * radius, // x coordinate of the corner
+                    Math.cos(angle) * radius // y coordinate of the corner
+            );
+        }
+    }
+
+    public static double rescaleCoordinate(double coordinate, double minValue, double maxValue, double desiredRange) {
+        return (coordinate - minValue) / (maxValue - minValue) * desiredRange;
     }
 }
