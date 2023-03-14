f1189167923de023e56454f6b1dd6de14f891b0f
Marco Sicklinger <you@example.com>
2023-03-14 12:34:03 +0400

    main push

--- src/main/java/utils/tile/Constants.java
+++ src/main/java/utils/tile/Constants.java
@@ -4,8 +4,11 @@ import java.util.List;
 
 public class Constants {
 
+    private Constants() {}
+
     public static final List<Integer> leftPathValues = List.of(2, 6, 7);
     public static final List<Integer> rightPathValues = List.of(3, 4, 8);
     public static final List<Integer> topPathValues = List.of(1, 5, 9);
+    public static final int BESTSCORE = 307;
 
 }
