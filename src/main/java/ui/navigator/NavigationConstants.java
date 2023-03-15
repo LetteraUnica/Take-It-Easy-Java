c63b44d94d5bc096d38a10d5c3c87daeed2c234d
Lettera <lorenzocav97@gmail.com>
2023-03-15 15:38:21 +0100

    Changed fxml names

--- src/main/java/ui/navigator/NavigationConstants.java
+++ src/main/java/ui/navigator/NavigationConstants.java
@@ -2,7 +2,7 @@ package ui.navigator;
 
 public class NavigationConstants {
     public static final String MAIN_MENU_FXML = "mainMenu.fxml";
-    public static final String LOBBY_FXML = "lobby.fxml";
-    public static final String MAIN_GAME_FXML = "mainGame.fxml";
+    public static final String LOBBY_FXML = "gameLobby.fxml";
+    public static final String MAIN_GAME_FXML = "gameBoard.fxml";
     public static final String GAME_OVER_FXML = "gameOver.fxml";
 }
